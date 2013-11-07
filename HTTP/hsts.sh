9fb349961a60cf6acff988fccf1c07b34ca7f608
connection <hacktalk@hacktalk.net>
2013-11-07 11:02:21 -0500

    Adds usage instructions for hsts.sh

--- HTTP/hsts.sh
+++ HTTP/hsts.sh
@@ -7,4 +7,11 @@ cat <<END
 	*            Luis Santana  *
 	****************************
 END
+
+if [ $# -ne 1 ]; then
+	echo Usage: $0 IP/Host
+	echo Note: no https:// is required
+	exit
+fi
+
 curl -skD - https://$1 |if grep "Strict-"; then echo "[+] HSTS Enabled For $1\n"; else echo "[!] HSTS Not Enabled For $1\n"; fi
