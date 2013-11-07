c894024c0f129fdfa3b9b3f671fa1aae4270ebdd
connection <hacktalk@hacktalk.net>
2013-11-07 10:59:26 -0500

    Adds usage instructions to open_ports.sh

--- SNMP/open_ports.sh
+++ SNMP/open_ports.sh
@@ -8,14 +8,20 @@ echo "	|        HackTalk Security        |"
 echo "	-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
 echo
 
-#TCP
+# Make sure we got enough arguments
+if [ $# -ne 1 ]; then
+   echo Usage: $0 IP
+   exit
+fi
+
+# TCP
 echo "Open TCP Ports"
 echo "------------------------"
 snmpwalk -cpublic $1 1.3.6.1.2.1.6.13.1.1 | while read i; do echo "[TCP] Port `echo $i |cut -d '.' -f 6` open"; done
 
 echo
 
-#UDP
+# UDP
 echo "Open UDP Ports"
 echo "------------------------"
 snmpwalk -cpublic $1 1.3.6.1.2.1.7.5.1.1 | while read i; do echo "[UDP] Port `echo $i |cut -d '.' -f 6|cut -d ' ' -f 1` open"; done
