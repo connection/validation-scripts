a6a51a94cbbf04777ac0d96997f9ed355964c0c2
connection <hacktalk@hacktalk.net>
2014-01-07 14:04:11 -0500

    Adds service enumeration to open_ports.sh

--- SNMP/open_ports.sh
+++ SNMP/open_ports.sh
@@ -27,3 +27,11 @@ echo "Open UDP Ports"
 echo "------------------------"
 echo "Command : snmpwalk -cpublic $1 1.3.6.1.2.1.7.5.1.1"
 snmpwalk -cpublic $1 1.3.6.1.2.1.7.5.1.1 | while read i; do echo "[UDP] Port `echo $i |cut -d '.' -f 6|cut -d ' ' -f 1` open"; done
+
+echo
+
+# Services
+echo "Running Services"
+echo "------------------------"
+echo "Command : snmpwalk -cpublic $1 1.3.6.1.4.1.77.1.2.3.1.1"
+snmpwalk -cpublic $1 1.3.6.1.4.1.77.1.2.3.1.1| while read i; do echo "[Service] `echo $i |cut -d'"' -f2` running"; done
